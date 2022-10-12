0a0a9db8db5b8abd007cc45d1b9aee3cb90adebb
ralfpoopo <ralfpoopo@gmail.com>
2022-10-12 11:18:14 +0000

    103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
+"""writing a docstring"""
 import math
 
 
 class MagicClass:
+    """set up the magic"""
 
-    """ Class that stores and manipulate the info of a circle
-        get the area and circunference"""
     def __init__(self, radius=0):
+        """ writing another docstring """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
-    """ get the area depending of the radius of the circle"""
     def area(self):
-        return ((self.__radius ** 2) * math.pi)
+        """again with the docstring"""
+        return self.__radius ** 2 * math.pi
 
-    """ get the circunference depending of the radius of the circle"""
     def circumference(self):
-        return (2 * math.pi * self.__radius)
+        """such docstring"""
+        return 2 * math.pi * self.__radius
