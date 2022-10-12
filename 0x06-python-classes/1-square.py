ea1838b8f3db3c6d4d06a603002d22c67c4398a8
ralfpoopo <ralfpoopo@gmail.com>
2022-10-12 10:39:15 +0000

    corrections

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """Represents a square"""
-
     def __init__(self, size):
-        """Initializing this square class
-        Args: size - represnets the size of the square defined
-        """
-
-        self.__size = size
+        self.__size = 
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,41 +1,25 @@
 #!/usr/bin/python3
 class Square:
-    """A class that represents a square"""
-
     def __init__(self, size=0):
-        """Initializing this square class
-        Args:
-            size: represnets the size of the square defined
-        Raises:
-            TypeError: if size is not integer
-            ValueError: if size is less than zero
-        """
-
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
-        self.__size = size
+        if isinstance(size, int) and size >= 0:
+            self.__size = size
+        elif (not isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
 
     @property
     def size(self):
-        """Retrieves size of square"""
-
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError('size must be an integer')
-        if value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
+        if isinstance(value, int) and value >= 0:
+            self.__size = value
+        elif (not isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
 
     def area(self):
-        """
-        Calculate area of the square
-        Returns: The square of the size
-        """
-
-        return (self.__size ** 2)
+        return (self.__size * self.__size)
